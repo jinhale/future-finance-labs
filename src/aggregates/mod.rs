ba25ba7f853b8bf96875cd99a47f03c7711af6bd
Justin Hale <haleleonj@outlook.com>
2020-12-05 21:32:03 -0500

    Fix bugs of aggregates

--- src/aggregates/mod.rs
+++ src/aggregates/mod.rs
@@ -2,17 +2,43 @@
 
 pub fn min(series: &[f64]) -> Option<f64> {
     if series.len() == 0 {
-        None()
-    } else {
-        series.iter().min()
+        return None;
+    }
+
+    let mut least = series.into_iter().nth(0).unwrap();
+
+    for f in series {
+        if f.is_nan() {
+            continue;
+        } else if f < least {
+            least = f;
+        }
+    }
+
+    match least {
+        x if !x.is_nan() => Some(*x),
+        _ => None,
     }
 }
 
 pub fn max(series: &[f64]) -> Option<f64> {
     if series.len() == 0 {
-        None()
-    } else {
-        series.iter().max()
+        return None;
+    }
+
+    let mut most = series.into_iter().nth(0).unwrap();
+
+    for f in series {
+        if f.is_nan() {
+            continue;
+        } else if f > most {
+            most = f;
+        }
+    }
+
+    match most {
+        x if !x.is_nan() => Some(*x),
+        _ => None,
     }
 }
 
@@ -22,7 +48,7 @@ pub fn max(series: &[f64]) -> Option<f64> {
 
 pub fn n_window_sma(n: usize, series: &[f64]) -> Option<Vec<f64>> {
     if series.len() == 0 || n == 0 {
-        return None();
+        return None;
     }
 
     let duration = if n > series.len() { series.len() } else { n };
@@ -31,7 +57,13 @@ pub fn n_window_sma(n: usize, series: &[f64]) -> Option<Vec<f64>> {
     let mut sma: Vec<f64> = vec![];
 
     while sma_ending <= series.len() {
-        sma.push(series[sma_beginning..sma_ending].iter().sum() / duration);
+        sma.push(series[sma_beginning..sma_ending].iter().fold(0.0, |acc, x| {
+            if x.is_nan() {
+                return acc;
+            }
+
+            return acc + x;
+        }) / (duration as f64));
         sma_beginning += duration;
         sma_ending += duration;
     }
@@ -41,22 +73,38 @@ pub fn n_window_sma(n: usize, series: &[f64]) -> Option<Vec<f64>> {
 
 pub fn price_diff(series: &[f64]) -> Option<(f64, f64)> {
     if series.len() < 2 {
-        return None();
+        return None;
     }
 
     // todo determine what relative price is
-    Some((series[0] - series[series.len() - 1], series[0] - series[series.len() - 1]))
+    Some((series[series.len() - 1] - series[0], series[series.len() - 1] - series[0]))
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn min_works() {
-        assert_eq!(vec![4.1, 5.1, 6.1, 7.1, 8.1, 9.1, -2.1], Some(-2.1));
+        let _vec = vec![4.1, 5.1, 6.1, 7.1, 8.1, 9.1, -2.1];
+        assert_eq!(min(&_vec), Some(-2.1));
     }
 
     #[test]
     fn max_works() {
-        assert_eq!(vec![-4.1, -5.1, 6.1, 7.1, -8.1, -9.1, 2.1], Some(6.1));
+        let _vec = vec![-4.1, -5.1, 6.1, 7.1, -8.1, -9.1, 2.1];
+        assert_eq!(max(&_vec), Some(7.1));
+    }
+
+    #[test]
+    fn n_window_sma_works() {
+        let test_vec: Vec<f64> = vec![1.3, 2.3, 3.3, 4.3, 5.3, 6.3, 7.3, 8.3, 9.3, 10.3];
+        assert_eq!(n_window_sma(5, &test_vec), Some(vec![3.3, 8.3]));
+    }
+
+    #[test]
+    fn price_diff_works() {
+        let test_vec: Vec<f64> = vec![1.3, 2.3, 3.3, 4.3, 5.3, 6.3, 7.3, 8.3, 9.3, 10.3];
+        assert_eq!(price_diff(&test_vec), Some((9.0, 9.0)));
     }
 }
