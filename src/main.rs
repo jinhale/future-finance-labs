4c1891fd387ce34c5b43a27ed9806d843627f7d0
Justin Hale <haleleonj@outlook.com>
2020-12-11 18:57:50 -0500

    Fix - Swap command line argument default values

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,14 @@ mod formatting;
 
 #[macro_use]
 extern crate clap;
-
-use clap::{Arg, App};
+use clap::App;
 
 fn main() {
     let yaml = load_yaml!("../cli.yml");
-    let matches = App::new("ffl")
-        .version("0.1.0")
-        .author("Justin Hale <jinhale@gmail.com>")
-        .about("Report market figures")
-        .arg(Arg::with_name("ticker")
-             .short("t")
-             .long("ticker")
-             .value_name("TICKER_CODE")
-             .help("Sets a custom config file")
-             .takes_value(true))
-        .arg(Arg::with_name("interval")
-             .short("i")
-             .long("interval")
-             .value_name("INTERVAL_CODE")
-             .help("The smallest unit of time to measure: 1m, 1h, 1d, 1m")
-             .takes_value(true))
-        .arg(Arg::with_name("duration")
-             .short("d")
-             .long("duration")
-             .value_name("DURATION_CODE")
-             .help("The range of time to consider")
-             .takes_value(true)).get_matches();
-
+    let matches = App::from_yaml(yaml).get_matches();
     let ticker = matches.value_of("ticker").unwrap_or("MSFT");
-    let interval = matches.value_of("interval").unwrap_or("30d");
-    let duration = matches.value_of("duration").unwrap_or("1d");
+    let interval = matches.value_of("interval").unwrap_or("1d");
+    let duration = matches.value_of("duration").unwrap_or("30d");
 
     let maybe_quotes = api_interface::get_quotes(ticker, interval, duration);
 
