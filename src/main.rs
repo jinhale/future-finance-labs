0562251cc6c5971991a4d733e6388f51d23e3d48
Justin Hale <haleleonj@outlook.com>
2020-12-11 19:00:34 -0500

    Refresh results every 30 seconds

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod aggregates;
 mod api_interface;
 mod formatting;
 
+use std::time::{Duration, Instant};
+use std::thread::sleep;
+
 #[macro_use]
 extern crate clap;
 use clap::App;
@@ -13,22 +16,27 @@ fn main() {
     let interval = matches.value_of("interval").unwrap_or("1d");
     let duration = matches.value_of("duration").unwrap_or("30d");
 
-    let maybe_quotes = api_interface::get_quotes(ticker, interval, duration);
+    loop {
+        let maybe_quotes = api_interface::get_quotes(ticker, interval, duration);
 
-    match maybe_quotes {
-        Ok(quotes) => {
-            if quotes.len() > 2 {
-                println!("{}", formatting::formatTitle());
-                println!("{}", formatting::formatQuote(&ticker, &quotes));
-            } else {
-                println!("{:?}", quotes[0]);
-                println!("quotes.len(): {}", quotes.len());
+        match maybe_quotes {
+            Ok(quotes) => {
+                if quotes.len() > 2 {
+                    println!("{}", formatting::formatTitle());
+                    println!("{}", formatting::formatQuote(&ticker, &quotes));
+                } else {
+                    println!("{:?}", quotes[0]);
+                    println!("quotes.len(): {}", quotes.len());
+                }
+            },
+            Err(e) => {
+                eprintln!("Error: {}", e);
+
+                std::process::exit(1);
             }
-        },
-        Err(e) => {
-            eprintln!("Error: {}", e);
+        };
 
-            std::process::exit(1);
-        },
+        // sleep for 30 seconds
+        sleep(Duration::new(30, 0));
     }
 }
