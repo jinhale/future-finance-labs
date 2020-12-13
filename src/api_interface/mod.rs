c1eec435e5855f4ed61845d56a235ef5164c409e
Justin Hale <haleleonj@outlook.com>
2020-12-13 14:02:46 -0500

    Preserve any YahooError from get_quotes

--- src/api_interface/mod.rs
+++ src/api_interface/mod.rs
@@ -4,13 +4,13 @@ use chrono::{Utc,TimeZone};
 use chrono::prelude::*;
 // use tokio_test;
 
-pub async fn get_quotes<'a>(ticker: &'a str, interval: &'a str, range: &'a str) -> std::result::Result<Vec<yahoo::Quote>, &'a str> {
+pub async fn get_quotes<'a>(ticker: &'a str, interval: &'a str, range: &'a str) -> std::result::Result<Vec<yahoo::Quote>, yahoo::YahooError> {
     let provider = yahoo::YahooConnector::new();
     let response = tokio_test::block_on(provider.get_quote_range(ticker, interval, range));
 
     match response {
         Ok(res) => Ok(res.quotes().unwrap()),
-        Err(e) => Err("Something went wrong. Are you connected to the internet? "),
+        Err(e) => Err(e),
     }
 }
 
